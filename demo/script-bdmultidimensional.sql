4cc8aa723280a784b56a4e4043d9a9b0fefdc922
Santiago <63568071+SantiagoSalinasbjj@users.noreply.github.com>
2024-10-29 02:42:35 -0300

    Update script-bdmultidimensional.sql
    corrección de la pk de de lugar de tipo int a tipo varchar del script "script-bdmultidimensional.sql"

--- demo/script-bdmultidimensional.sql
+++ demo/script-bdmultidimensional.sql
@@ -1,9 +1,11 @@
+USE BD_MULTIDIMENSIONAL;
 
 CREATE TABLE Lugar (
-    Lugar_id INT PRIMARY KEY,
+    Lugar_id VARCHAR(50) PRIMARY KEY,  
     Nombre_Barrio VARCHAR(255),
     Comuna SMALLINT
 );
+
 
 CREATE TABLE Tiempo (
     Tiempo_id INT PRIMARY KEY,
@@ -14,6 +16,7 @@ CREATE TABLE Tiempo (
     Año SMALLINT
 );
 
+
 CREATE TABLE Tipo_Incidente (
     Tipo_incidente_id INT PRIMARY KEY,
     Descripcion VARCHAR(255)
@@ -23,7 +26,7 @@ CREATE TABLE Tipo_Incidente (
 CREATE TABLE Tiempo_Lugar (
     Tiempo_Lugar_id INT PRIMARY KEY,
     Tiempo_id INT,
-    Lugar_id INT,
+    Lugar_id VARCHAR(50),  
     FOREIGN KEY (Tiempo_id) REFERENCES Tiempo(Tiempo_id),
     FOREIGN KEY (Lugar_id) REFERENCES Lugar(Lugar_id)
 );
@@ -31,7 +34,7 @@ CREATE TABLE Tiempo_Lugar (
 
 CREATE TABLE Incidente (
     id_incidente INT PRIMARY KEY,
-    Lugar_id INT,
+    Lugar_id VARCHAR(50), 
     Tiempo_id INT,
     Tiempo_Lugar_id INT,
     Tipo_incidente_id INT,
@@ -40,3 +43,5 @@ CREATE TABLE Incidente (
     FOREIGN KEY (Tiempo_Lugar_id) REFERENCES Tiempo_Lugar(Tiempo_Lugar_id),
     FOREIGN KEY (Tipo_incidente_id) REFERENCES Tipo_Incidente(Tipo_incidente_id)
 );
+
+
